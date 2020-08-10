98ae34a7b680adbab5294510b7ca23d93d956593
Nathan Cowley <nathancowley12@gmail.com>
2020-08-10 16:54:36 -0600

    mvp

--- router/accounts.js
+++ router/accounts.js
@@ -20,19 +20,63 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     const accountId = req.params.id;
 
-    
+    db('accounts')
+    .where({ id: accountId })
+    .then(account => {
+        res.status(200).json(account)
+    })
+    .catch(error => {
+        res.status(500).json({ error: error.message })
+    })
 })
 
 router.post('/', (req, res) => {
-    
+    const account = req.body;
+
+    db('accounts')
+        .insert(account)
+        .returning('id')
+        .then(ids => {
+            res.status(201).json({ inserted: ids })
+        })
+        .catch(error => {
+            res.status(500).json({ error: error.message })
+        })
 })
 
 router.put('/:id', (req, res) => {
+    const changes = req.body;
+    const accoundId = req.params.id;
+
+    db('accounts')
+    .where({ id: accoundId })
+    .update(changes)
+    .then(count => {
+        if(count){
+            res.status(200).json({ message: 'updated successfully' })
+        }else{
+            res.status(404).json({ message: 'not found' })
+        }
+    })
     
 })
 
 router.delete('/:id', (req, res) => {
-    
+    const accoundId = req.params.id;
+
+    db('accounts')
+    .where({ id: accoundId })
+    .del()
+    .then(account => {
+        if(acccount){
+            res.status(200).json({ message: 'removed successfully' })
+        }else{
+            res.status(404).json({ message: 'request not found' })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ error: error.message })
+    })
 })
 
 
